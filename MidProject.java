a10a48e38877d1d56bd481841f7acdee8907d217
Angel Utrilla <69911720+FlikingPie@users.noreply.github.com>
2026-03-17 12:38:34 -0500

    Create MidProject.java

---
+++ MidProject.java
@@ -0,0 +1,11 @@
+package com.mycompany.midproject;
+
+/**
+ *
+ * @author CLIENTE
+ */
+public class MidProject {
+    public static void main(String[] args) {
+        new UsuarioFrame().setVisible(true);
+    }
+}
